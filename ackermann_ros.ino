4aa367d4f93ce33d50fcc28131a6419d103411dc
Dr. Benjamin Bird <benjamin.bird4392@gmail.com>
2021-05-02 15:05:38 +0100

    untested

--- ackermann_ros.ino
+++ ackermann_ros.ino
@@ -4,11 +4,16 @@
 #include <geometry_msgs/Twist.h>
 #include <tf/transform_broadcaster.h>
 #include <nav_msgs/Odometry.h>
+#include <math.h>
 
 #include <BTS7960.h>
 #include <Encoder.h>
 #include <PID_v1.h>
 #include <Servo.h>
+
+#define wheel_base_length 0.34
+#define wheel_base_width 0.22
+#define wheel_circumfrance 0.4273
 
 double last_time = micros();
 double tick_time = 0;
@@ -16,13 +21,8 @@ long old_position = 0;
 
 double Setpoint, Output;
 double Input = 0;
-double Kp=1, Ki=0.5, Kd=0.01;
 
 bool twist_flag = false;
-
-double wheel_base_length = 0.34;
-double wheel_base_width = 0.22;
-double wheel_circumfrance = 0.4273;
 
 int twist = 0; // degrees
 int vel = 0; // rpm
@@ -30,19 +30,26 @@ int vel = 0; // rpm
 double x_pos = 0.0;
 double y_pos = 0.0;
 double current_yaw = 0.0;
+unsigned long p_time = millis();
 
 
 BTS7960 motorController(6, 4, 5, 11); //en_l en_r l_pwm r_pwm
 Encoder myEnc(2, 3);
-PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+
+PID myPID(&Input, &Output, &Setpoint, 1.0, 0.5, 0.01, DIRECT); //P,I,D
 Servo myservo;
 
 ros::NodeHandle  nh;
 
-ros::Subscriber<geometry_msgs::Twist> sub("/cmd_vel", cmd_vel_cb);
+//nav_msgs::Odometry odom;
+//ros::Publisher odom_pub("odom", &odom);
 
 ros::Publisher odom_pub = nh.advertise<nav_msgs::Odometry>("odom", 50);
+
+
 tf::TransformBroadcaster odom_broadcaster;
+
+
 
 double calculate_rpm(){
    long new_position = myEnc.read();
@@ -76,19 +83,6 @@ double covert_vel_rpm(double vel){
   return RPM;
 }
 
-void setup(){
-    input_vel.reserve(100);
-    input_twist.reserve(100);
-    myservo.attach(9);
-    myservo.write(90); // 57 = full left, 123 = full right, 90 = middle.
-    motorController.Enable();
-    Setpoint = 0;
-    myPID.SetSampleTime(20);
-    myPID.SetMode(AUTOMATIC);
-    myPID.SetOutputLimits(-255,255);
-    nh.initNode();
-    nh.subscribe(sub);
-}
 
 
 void cmd_vel_cb(const geometry_msgs::Twist& cmd_vel_msg){
@@ -97,56 +91,63 @@ void cmd_vel_cb(const geometry_msgs::Twist& cmd_vel_msg){
     //convert steering angle from radians to degrees
     vel  = int((cmd_vel_msg.linear.x / 0.4273) * 60);
     
-    if (vel > 0); // only if vehicle is actually moving, as needs forward velocity to turn
+    if (vel > 0){// only if vehicle is actually moving, as needs forward velocity to turn
         twist = int(cmd_vel_msg.angular.z * 57.296);//convert to degrees
-    else;
+    }
+    else{
         twist = 0;
+    }    
 }
 
 void calculate_odom(){
     
     //probably getting degrees and radians mixed up a bit here
     
-    float speed, x_vel, y_vel;
+    float velocity, x_vel, y_vel;
+    float dt;
             
-    speed = (vel / 60) * 0.4273;
+    velocity = (vel / 60) * 0.4273;
     
-    x_vel = speed * math.cos(current_yaw);
-    y_vel = speed * math.sin(current_yaw);
-    
-    ros::Duration dt = time_duration;
-    
-    x_pos += x_dot * dt.toSec();
-    y_pos += y_dot * dt.toSec();
+    x_vel = velocity * cos(current_yaw);
+    y_vel = velocity * sin(current_yaw);
 
-    current_yaw += twist * dt.toSec();
+    dt = (millis() - p_time)/1000;
+    
+    x_pos += x_vel * dt;
+    y_pos += y_vel * dt;
+
+    current_yaw += twist * dt;
     
     
-    
-    geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(current_yaw);
-     //first, we'll publish the transform over tf
     geometry_msgs::TransformStamped odom_trans;
-    odom_trans.header.stamp = current_time;
+    odom_trans.header.stamp = nh.now();
     odom_trans.header.frame_id = "odom";
     odom_trans.child_frame_id = "base_link";
   
     odom_trans.transform.translation.x = x_pos;
     odom_trans.transform.translation.y = y_pos;
     odom_trans.transform.translation.z = 0.0;
-    odom_trans.transform.rotation = odom_quat;
- 
+    odom_trans.transform.rotation.x = 0.0;
+    odom_trans.transform.rotation.y = 0.0;
+    odom_trans.transform.rotation.z = sin(current_yaw/2.0); // cheating a bit here as "createQuaternionMsgFromYaw" isn't available in arduino
+    odom_trans.transform.rotation.w = cos(current_yaw/2.0);
+
     //send the transform
-    odom_broadcaster.sendTransform(odom_trans);
+    //odom_broadcaster.sendTransform(odom_trans);
     //next, we'll publish the odometry message over ROS
     nav_msgs::Odometry odom;
-    odom.header.stamp = current_time;
+    odom.header.stamp = nh.now();
     odom.header.frame_id = "odom";
   
     //set the position
     odom.pose.pose.position.x = x_pos;
     odom.pose.pose.position.y = y_pos;
     odom.pose.pose.position.z = 0.0;
-    odom.pose.pose.orientation = odom_quat;
+    odom.pose.pose.orientation.x = 0.0;
+    odom.pose.pose.orientation.y = 0.0;
+    odom.pose.pose.orientation.z = sin(current_yaw/2.0);
+    odom.pose.pose.orientation.w = cos(current_yaw/2.0);
+    
  
     //set the velocity
     odom.child_frame_id = "base_link";
@@ -155,14 +156,37 @@ void calculate_odom(){
     odom.twist.twist.angular.z = twist;
 
     //publish the message
-    odom_pub.publish(odom);
+    odom_pub.publish(&odom);
+    
+    p_time = millis();
 }
 
 
 
+
+
+
+void setup(){
+
+    myservo.attach(9);
+    myservo.write(90); // 57 = full left, 123 = full right, 90 = middle.
+    motorController.Enable();
+    Setpoint = 0;
+    myPID.SetSampleTime(20);
+    myPID.SetMode(AUTOMATIC);
+    myPID.SetOutputLimits(-255,255);
+
+    
+    nh.initNode();
+    ros::Subscriber<geometry_msgs::Twist> sub("/cmd_vel", cmd_vel_cb);
+    nh.subscribe(sub);
+
+}
+
+
 void loop(){
     
-    myservo.write(convet_steering_angle(twist,vel));
+    myservo.write(convert_steering_angle(twist,vel));
         
     Setpoint = vel;
     Input = calculate_rpm();
@@ -177,5 +201,3 @@ void loop(){
         
     nh.spinOnce();
 }
-
-
